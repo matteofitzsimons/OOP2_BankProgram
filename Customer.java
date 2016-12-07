413f3250eaea0d087f0f670564237a55387d82ce
matteofitzsimons <matteofitz@yahoo.ie>
2016-12-07 01:24:12 +0000

    no message

--- Customer.java
+++ Customer.java
@@ -1,4 +1,13 @@
-
+/**
+ * 
+ * @author Matteo
+  	 * Setting attributes for Customer class 
+ 	 * @param name
+	 * @param dob
+	 * @param bankPin
+	 * @param bankNumber
+	 * @param balance
+ */
 public class Customer {
 	//setting attributes
 	private String name;
@@ -7,8 +16,15 @@ public class Customer {
 	private int bankNumber;
 	private double balance;
 
-	
-	//constructer
+/**
+	 * 	Constructor - Assigning default values to the set attributes
+	 * @param name
+	 * @param dob
+	 * @param bankPin
+	 * @param bankNumber
+	 * @param balance
+ */
+	//Constructor
 	public Customer(){
 		setName("unknown");
 		setDob("unkown");
@@ -17,7 +33,14 @@ public class Customer {
 		setBalance(0);
 
 	}
-		
+	/**
+	 * Multiple argument constructor for Customer class
+	 * @param name
+	 * @param dob
+	 * @param bankPin
+	 * @param bankNumber
+	 * @param balance
+	 */
 	public Customer(String name, String dob, int bankPin, int bankNumber, double balance)
 	{
 		this.name = name;
@@ -26,49 +49,95 @@ public class Customer {
 		this.bankNumber = bankNumber;
 		this.balance = balance;
 	}
+
+	/**
+	 * mutator method for name
+	 * @param name
+	 */
 	//mutator methods
 	public void setName(String name)
 	{
 		this.name = name;
 	}
+	/**
+	 * mutator method for dob
+	 * @param dob
+	 */
 	public void setDob(String dob)
 	{
 		this.dob = dob;
 	}
+	/**
+	 * mutator method for bankPin
+	 * @param bankPin
+	 */
 	public void setBankPin(int bankPin)
 	{
 		this.bankPin = bankPin;
 	}
+	/**
+	 * mutator method for bankNumber
+	 * @param bankNumber
+	 */
 	public void setBankNumber(int bankNumber)
 	{
 		this.bankNumber = bankNumber;
 	}
+	/**
+	 * Mutator method for balance
+	 * @param balance
+	 */
 	public void setBalance(double balance)
 	{
 		this.balance = balance;
 	}
 	
 	
+	/**
+	 *  Accessor method for name
+	 * @return String name
+	 */	
 	//accessor method
 	public String getName()
 	{
 		return name;
 	}
+	/**
+	 * Accessor method for dob
+	 * @return String dob
+	 */
 	public String getDob()
 	{
 		return dob;
 	}
+	/**
+	 * Accessor method for bankPin
+	 * @return int bankPin
+	 */
 	public int getBankPin()
 	{
 		return bankPin;
 	}
+	/**
+	 *Accessor method for ankNumber 
+	 * @return int bankNumber
+	 */
 	public int getBankNumber(){
 		return bankNumber;
 	}
+	/**
+	 * Accessor method for balance
+	 * @return double balance
+	 */
 	public double getBalance()
 	{
 		return balance;
 	}
+
+/**
+ * toString method for Customer class
+ * @return getName, getDob, getBankPin, getBankNumber, getBalance
+ */
 	
 	//to string method
 	public String toString()
@@ -76,6 +145,7 @@ public class Customer {
 		return "name: "+getName()+"\nDate Of Birth:"+getDob() +"\nBank Pin:"+getBankPin()+"\nBank Number:"+getBankNumber()+"\nBalance:%.2f"+getBalance();
 		
 	}
+	
 
 	
 
